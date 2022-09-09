3f1f760ac3fbe74f7295ccc65fdfd412c6c57c75
Kranthi <111005858+Kranthi077@users.noreply.github.com>
2022-09-09 13:26:46 +0530

    variables and datatypes

---
+++ src/test/java/coreJava/VariablesAndDataTypes.java
@@ -0,0 +1,41 @@
+package coreJava;
+
+public class VariablesAndDataTypes {
+
+	public static void main(String[] args) {
+
+		// a is variable here & 5 is its value
+		// int is a datatype
+		int a = 5 + 10;
+		System.out.println(a);
+		a = 17;
+		System.out.println(a);
+
+		a = (int) 19.5;
+		System.out.println(a);
+
+		float b = 7.5f;
+		System.out.println(b);
+		
+		float c = 7.5444444f;
+		System.out.println(c);
+		
+		
+		double d = 7.544444444444555;
+		System.out.println(d);
+		
+		double f = 7544444444444555.0;
+		System.out.println(f);
+		String text = "Good Morning! Amazon logo is present";
+		String text1 = "Good Morning! Amazon logo is not present";
+		
+		boolean value = true;
+		boolean value1 = false;
+		System.out.println(value);
+		System.out.println(text);
+		System.out.println(value1);
+		System.out.println(text1);
+
+	}
+
+}
