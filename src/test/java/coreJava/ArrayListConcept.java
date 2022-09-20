25c2d71de23a88d8107ba7cec21dd324e98c06f1
Kranthi <111005858+Kranthi077@users.noreply.github.com>
2022-09-20 12:13:42 +0530

    Strings and arraylists

---
+++ src/test/java/coreJava/ArrayListConcept.java
@@ -0,0 +1,42 @@
+package coreJava;
+
+import java.util.ArrayList;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+		// Syntax of ArrayList
+
+		/*
+		 * Syntax to create object for a class
+		 * 
+		 * ClassName object1Name = new ClassName();
+		 */
+
+		ArrayList a = new ArrayList();
+		a.add("Selenium");
+		a.add("training");
+		a.add(1);
+		a.add(78);
+		a.add('c');
+
+		for (int i = 0; i < a.size(); i++) {
+			System.out.println(a.get(i));
+		}
+
+		a.remove(2);
+
+		System.out.println("After removing 2nd element");
+		for (int i = 0; i < a.size(); i++) {
+			System.out.println(a.get(i));
+		}
+
+		// Generic
+		/*
+		 * ArrayList<String> a1 = new ArrayList<String>(); a1.add("Selenium");
+		 * a1.add("training");
+		 */
+
+	}
+
+}
---
+++ src/test/java/coreJava/StringsConcept.java
@@ -0,0 +1,35 @@
+package coreJava;
+
+import java.util.Iterator;
+
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
+public class StringsConcept {
+
+	public static void main(String[] args) {
+		// String is not a dataTypes or Primitive dataType. String is basically a class
+		
+		// 1st way of declaring by new operator
+		String str = new String("Welcome to string");
+		String str3 = new String("Welcome to string");
+		System.out.println(str);
+		System.out.println(str3);
+
+		// 2nd way of doing is by not using new : By String Literals
+		String str1 = "Welcome to String Literals";
+		String str2 = "Welcome to String Literals";
+		System.out.println(str1);
+		System.out.println(str2);
+
+		// breaking point is called as delimiter
+		String[] splittedString = str1.split(" ");
+
+		for (int i = 0; i < splittedString.length; i++) {
+			System.out.println(splittedString[i]);
+		}
+
+		for (int i = 0; i < str1.length(); i++) {
+			System.out.println(str1.charAt(i));
+		}
+	}
+}
